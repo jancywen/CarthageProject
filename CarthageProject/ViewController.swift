ed1c13be93b9f67a4fadead23bcc6725f22fe40f
wangwenjie <jancywen@163.com>
2020-07-19 23:14:00 +0800

    init project

---
+++ CarthageProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageProject
+//
+//  Created by wangwenjie on 2020/7/19.
+//  Copyright © 2020 wangwenjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
